ed80d0765ad0178f6ab2b75cf558cef7cf213aa5
aslan-asilon3 <sulaslansetiawan3@gmail.com>
2023-05-10 01:31:57 -0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+
+    // Route::resource('/products', ProductController::class);
+    // Route::get('/cart', function () {
+    //     return view('visitor.cart');
+    // });
+    Route::get('cart', [CartController::class, 'cartList'])->name('cart.list');
+    Route::post('cart', [CartController::class, 'addToCart'])->name('cart.store');
+    Route::post('update-cart', [CartController::class, 'updateCart'])->name('cart.update');
+    Route::post('remove', [CartController::class, 'removeCart'])->name('cart.remove');
+    Route::post('clear', [CartController::class, 'clearAllCart'])->name('cart.clear');
+    
+    Route::get('products', [ProductController::class, 'productList'])->name('products.list');
+});
+
+
+
